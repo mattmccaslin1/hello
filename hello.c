7b9a14223be0dcc1aef8927b0c695a9ab0741d85
Matthew McCaslin <mmccaslin2@huskers.unl.edu>
2021-08-27 01:00:50 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Matthew McCaslin
+ *         mmccaslin2@huskers.unl.edu
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Matthew McCaslin\n");
+
+  return 0;
+}
