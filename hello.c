72f82fa61abf3f7108100cfffc693d19791c5b9f
Eli Martinez <emartinez19@huskers.unl.edue>
2021-08-27 02:04:42 +0000

    Partner Edit

--- hello.c
+++ hello.c
@@ -12,8 +12,9 @@
 int main(int argc, char **argv) {
 
   printf("Matthew McCaslin!\n");
-  printf("I am a computer engineering major.");
-  
+  printf("I am a computer engineering major.\n");
+  printf("Eli Martinez\n");
+  printf("Computer Engineering\n");
 
   return 0;
 }
